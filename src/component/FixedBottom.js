150f9d941eb8442ab7154d13af88953ef67b849b
sasumi <cnsasumi@gmail.com>
2019-06-10 09:22:01 +0800

    Fixed bottom component

---
+++ src/component/FixedBottom.js
@@ -0,0 +1,33 @@
+/**
+ * 底部固定
+ */
+define('ywj/FixedBottom',function(require){
+	var Util = require('ywj/util');
+	var cls = 'fixed-bottom';
+	var css = '.'+cls+' {position:fixed; bottom:0; background-color:white}';
+	$('<style>'+css+'</style>').appendTo($('head'));
+
+	return {
+		nodeInit: function($node){
+			var height = $node.height();
+			var width = $node.width();
+			var outer_height = $node.outerHeight();
+			var outer_width = $node.outerWidth();
+			var top = $node.offset().top;
+			var $shadow = $('<'+$node[0].nodeName+'>');
+			$shadow.css('visibility', 'hidden').width(outer_width).height(outer_height).hide();
+			$shadow.insertAfter($node);
+			$(window).scroll(function(){
+				var scroll_top = $(this).scrollTop();
+				var vh = Util.getRegion().visibleHeight;
+				if(scroll_top+vh > (top+outer_height)){
+					$node.removeClass(cls).removeAttr('height').removeAttr('width');
+					$shadow.hide();
+				} else {
+					$node.addClass(cls).height(height).width(width);
+					$shadow.show();
+				}
+			}).trigger('scroll');
+		}
+	}
+});
