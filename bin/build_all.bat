ae6f714e3fc46c4323a93a790242585a7f111aef
sasumi <cnsasumi@gmail.com>
2019-06-10 12:52:31 +0800

    fixed source map

--- bin/build_all.bat
+++ bin/build_all.bat
@@ -1,8 +1,18 @@
 @echo off
 echo Building dist/ywj.min.js
-java -jar closure-compiler.jar --js ../src/component/*.js --js_output_file ../dist/ywj.min.js --create_source_map ../dist/ywj.sourcemap.js --compilation_level WHITESPACE_ONLY
+java -jar closure-compiler.jar ^
+	--js ../src/component/*.js ^
+	--create_source_map ../dist/ywj.min.map ^
+	--source_map_format V3 ^
+	--compilation_level WHITESPACE_ONLY ^
+	--js_output_file ../dist/ywj.min.js ^
+	--output_wrapper "%%output%%//#sourceMappingURL=../dist/ywj.min.map"
 
 echo Building dist/ywj.debug.js
-java -jar closure-compiler.jar --js ../src/component/*.js --js_output_file ../dist/ywj.debug.js --compilation_level BUNDLE
+java -jar closure-compiler.jar ^
+	--js ../src/component/*.js ^
+	--js_output_file ../dist/ywj.debug.js ^
+	--compilation_level BUNDLE
+
 echo Done
 pause
